2d0131f244351af6b7e0265bd23d50e6fa6368a4
Mauricio <Mauricio@DESKTOP-N1Q25V5>
2019-06-30 02:32:45 -0500

    segunda version

---
+++ ing_software_mgpi/src/ing_software_mgpi/Ing_software_mgpi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_software_mgpi;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Ing_software_mgpi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Proyecto conexion github netbeans");
+    }
+    
+}
